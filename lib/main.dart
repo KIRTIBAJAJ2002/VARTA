53a94514a6bdabe41e062087752b100fa543c3cc
Kirti Bajaj <115650796+KIRTIBAJAJ2002@users.noreply.github.com>
2025-03-09 11:05:56 +0000

    1st page

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,38 @@ class MyApp extends StatelessWidget {
         body: Container(
           decoration: AppTheme.backgroundGradient,
           child: Center(
-            child: Lottie.asset('assets/varta1.json'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset('assets/varta1.json'),
+                const SizedBox(height: 40), // Add some space between the animation and the button
+                Column(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        // Handle button press here
+                        print('Get Started');
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.transparent, // Make the button background transparent
+                        shadowColor: Colors.transparent, // Remove the default shadow
+                        padding: const EdgeInsets.all(0), // Remove default padding
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap, // Make the tap target as small as possible
+                      ),
+                      child: const Text(
+                        'Get Started ',
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                    Lottie.asset(
+                      'assets/wave.json', // Use your Lottie file for the button background
+                      width: 200, // Adjust the width to fit your button size
+                      height: 60, // Adjust the height to fit your button size
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
